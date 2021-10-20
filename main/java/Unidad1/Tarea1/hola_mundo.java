fda0db1a866a6f67ab07571db14be5ff6dd7e02a
alftacmir <91072220+alftacmir@users.noreply.github.com>
2021-10-20 16:44:18 +0200

    Delete main/java/Unidad1 directory

--- main/java/Unidad1/Tarea1/hola_mundo.java
+++
@@ -1,8 +0,0 @@
-package Unidad1.Tarea1;
-
-public class hola_mundo {
-    public static void main(String[] args) {
-        System.out.println("Hola Mundo!!!");
-    }
-
-}
--- main/java/Unidad1/Tarea8_Parte2/Ej_pintar.java
+++
@@ -1,7 +0,0 @@
-package Unidad1.Tarea8_Parte2;
-
-public class Ej_pintar {
-  public static void main(String[] args) {
-    //
-  }
-}
