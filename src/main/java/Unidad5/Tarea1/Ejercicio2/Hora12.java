a2a51f60f6b226ad7c37063b687c513759b6dfe5
alftacmir <alum.ataconm.2021@iesalixar.org>
2022-02-03 18:13:40 +0100

    Hora12 poner un sethoras nuevo y aprovechar codigo

---
+++ src/main/java/Unidad5/Tarea1/Ejercicio2/Hora12.java
@@ -0,0 +1,16 @@
+package Unidad5.Tarea1.Ejercicio2;
+
+import Unidad5.Tarea1.Ejercicio1.Hora;
+
+public class Hora12 extends Hora {
+
+    private String am;
+
+    public Hora12(int hora, int minutos,String am) {
+        super(hora, minutos);
+        this.am=am;
+    }
+
+
+
+}
---
+++ src/main/java/Unidad5/Tarea1/Ejercicio2/Main.java
@@ -0,0 +1,10 @@
+package Unidad5.Tarea1.Ejercicio2;
+
+public class Main {
+  public static void main(String[] args) {
+
+    Hora12 h1=new Hora12(11,35,"pm");
+    System.out.println(h1);
+  }
+
+}
