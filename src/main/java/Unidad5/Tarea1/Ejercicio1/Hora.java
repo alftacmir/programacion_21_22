8f9d96b4cd0e139a06821fc056bd58a6f0e29d36
alftacmir <alum.ataconm.2021@iesalixar.org>
2022-02-03 18:12:42 +0100

    Hora supertipo

---
+++ src/main/java/Unidad5/Tarea1/Ejercicio1/Hora.java
@@ -0,0 +1,56 @@
+package Unidad5.Tarea1.Ejercicio1;
+
+public class Hora {
+    protected int hora;
+    protected int minutos;
+
+    public Hora(int hora, int minutos) {
+        setHora(hora);
+        setMinutos(minutos);
+    }
+
+    public Hora() {
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+        while (hora>=24){
+            hora-=24;
+        }
+        this.hora = hora;
+    }
+
+    public int getMinutos() {
+        return minutos;
+    }
+
+    public void setMinutos(int minutos) {
+        while (minutos>=60){
+            minutos-=60;
+            this.hora++;
+        }
+        while (this.hora>=24){
+            this.hora-=24;
+        }
+        this.minutos = minutos;
+    }
+
+    public void inc(){
+        this.minutos++;
+        if (this.minutos>=60){
+            this.minutos-=60;
+            this.hora++;
+        }
+        if (this.hora>=24){
+            this.hora-=24;
+        }
+    }
+
+    @Override
+    public String toString(){
+        return "La hora actual es "+getHora()+":"+getMinutos();
+    }
+}
---
+++ src/main/java/Unidad5/Tarea1/Ejercicio1/Main.java
@@ -0,0 +1,8 @@
+package Unidad5.Tarea1.Ejercicio1;
+
+public class Main {
+  public static void main(String[] args) {
+    Hora h1=new Hora(24,60);
+    System.out.println(h1);
+  }
+}
